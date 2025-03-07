fe2ecb334db10df00ed532e6c9985d5a112c4f3d
Rakshithss2000 <rakshithss2000@gmail.com>
2025-03-07 18:32:59 +0530

    Update backend.tf

--- devops/backend.tf
+++ devops/backend.tf
@@ -5,7 +5,7 @@ terraform {
     key    = "terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
-    dynamodb_table = "terraform-state-lock-dynamo"
+    dynamodb_table = "terraform"
     }
   }
 
