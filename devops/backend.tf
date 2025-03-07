1f7537dd3e4d9d46f72b060c8128d22558e53c8a
Rakshithss2000 <rakshithss2000@gmail.com>
2025-03-07 18:23:40 +0530

    Update backend.tf

--- devops/backend.tf
+++ devops/backend.tf
@@ -1,8 +1,8 @@
 
 terraform {
   backend "s3" {
-    bucket = "jms43-terraform-backend"
-    key    = "jmsth_jenkins.tfstate"
+    bucket = "rockstar123"
+    key    = "terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
     dynamodb_table = "terraform-state-lock-dynamo"
